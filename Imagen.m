dc43d8cd8998da99599cb2d3f354fdfcca392f72
Dioney <dioneyblanco@gmail.com>
2015-04-17 15:21:34 -0500

    Commit some things that were missing

--- Imagen.m
+++ Imagen.m
@@ -1,52 +1,52 @@
 %--------------------------------------------------------
 % Carlos Dioney Blanco González         131370
 % Course: Advanced Linear Algebra  Spring 2015
-% Imagen scriptfile for making image aproaches with
-% principal components analyisis.
+% Imagen scriptfile for making image approaches with
+% principal component analyisis for 3 different ranks
 %--------------------------------------------------------
 
 imagen    = input('Cómo se llama tu archivo? (con extensión) \n','s'); 
 X         = imread(imagen);   % Reads image
 X         = double(X);        % Convert to double precision
 [U, S, V] = svd(X,0);         % Obtain SVD decomposition
-sigma     = diag(S);          % We take diagonal of S
+sigma     = diag(S);          % We take the diagonal of S
 r         = rank(X);          % rank of original matrix
 
 flag_1 = -1; flag_2 = -1; flag_3 = -1;  
 
-% First aproximation
+% First approximation
 while flag_1 < 0
     fprintf('Rango de tu primera aproximación, menor a %5.3i \n',r)
-    r_1 = input('');  % Receive desired rank for first aproximation
+    r_1 = input('');  % Receive desired rank for first approximation
   if r_1 < r          % we can't exceed rank of the matrix
   flag_1 = 0;
   end      
 end
-X1 = componentes_principales(X,r_1);
-imwrite(uint8(X1), 'Aprox_1.jpg');
-subplot(2,2,1); 
-image(X1)
+X1 = Principal_Components(X,r_1);   % Use Principal Components to get an approach
+imwrite(uint8(X1), 'Aprox_1.jpg');  % Save first approximation
+subplot(2,2,1);                     % Position first image
+image(X1)                           % Display
 colormap(gray(256))
 axis image, axis off
 title(['rango = ' int2str(r_1)]);
 
-% Segunda aproximación
+% Second approximation
 while flag_2 < 0
     fprintf('Rango de tu segunda aproximación, menor a %5.3i \n',r)
-    r_2 = input(''); % Receive desired rank for second aproximation
+    r_2 = input(''); % Receive desired rank for second approximation
   if r_2 < r         % we can't exceed rank of the matrix
   flag_2 = 0;
   end      
 end
-X10 = componentes_principales(X,r_2);
-imwrite(uint8(X10), 'Aprox_2.jpg');
-subplot(2,2,2);
-image(X10)
+X2 = componentes_principales(X,r_2); % Use Principal Components to get an approach
+imwrite(uint8(X2), 'Aprox_2.jpg');   % Save second approximation
+subplot(2,2,2);                      % Position second image
+image(X2)                            % Display
 colormap(gray(256))
 axis image, axis off
 title(['rango = ' int2str(r_2)]);
 
-% Tercer aproximación
+% Third approach
 while flag_3 < 0
     fprintf('Rango de tu tercera aproximación, menor a %5.3i \n',r)
     r_3 = input('');     % Receive desired rank for third aproximation
@@ -54,17 +54,17 @@ while flag_3 < 0
   flag_3 = 0;
   end      
 end
-X30 = componentes_principales(X,r);
-imwrite(uint8(X30), 'Aprox_3.jpg');
-subplot(2,2,3);
-image(X30)
+X3 = componentes_principales(X,r); % Use Principal Components to get an approach
+imwrite(uint8(X3), 'Aprox_3.jpg'); % Save third approximation
+subplot(2,2,3);                    % Position second image
+image(X3)                          % Display
 colormap(gray(256))
 axis image, axis off
 title(['rango = ' int2str(r_3)]);
 
 % Imagen original
-subplot(2,2,4);
-image(X)
+subplot(2,2,4);                 % Position original matrix 
+image(X)                        % Display
 colormap(gray(256))
 axis image, axis off
 r = rank(X);
