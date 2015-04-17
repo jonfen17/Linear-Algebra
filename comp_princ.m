29acbfda9f9fd4fe4a2e46ffba74683498871b30
Dioney <dioneyblanco@gmail.com>
2015-04-17 15:39:29 -0500

    Commit some explanation of Principal Components

--- comp_princ.m
+++ comp_princ.m
@@ -1,19 +1,21 @@
 %--------------------------------------------------------------------------
-% Carlos Dioney Blanco González         131370
-% Curso: Álgebra Lineal Avanzada  Primavera 2015
+% Carlos Dioney Blanco GonzÃ¡lez         131370
+% Course - Advanced Linear Algebra   Spring 2015
 %
-% Este programa realiza una descomposición de la matriz A como suma de r
-% ( r <= rank( A ) ) matrices E de rango 1. Cada E se obtiene a partir de 
-% la svd  como E_i = S(i,i)*U(:,i)*V(:,i)'  S diagonal U,V unitarias
+% This program makes an approximation of the matrix A as sum of r matrixes 
+% E of rank 1, where  r < rank( A ). Each E is obtained from the SVD 
+% decomposition as it follows:
+%
+%   E_i = S(i,i)*U(:,i)*V(:,i)' ... (1)     S diagonal U,V unitary
 %
 % Input:
-% A - Matriz original
-% r - número de matrices de rango 1 para construir la descomposición
+% A - Original matrix
+% r - Number of rank 1 matrixes to build the approach
 % Output: 
-% B - suma de r matrices de rango 1
+% B - Sum of r matrixes of rank 1 obtained from (1)
 % 
 %--------------------------------------------------------------------------
-function [ B ] = comp_princ( A, r )
+function [ B ] = Principal_Components( A, r )
 
 B       = [];
 [U,S,V] = svd(A);
